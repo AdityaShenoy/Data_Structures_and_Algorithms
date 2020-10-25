e443afee688325f87b552ca7e671360ac8a92c93
AdityaShenoy <adityashenoy49@gmail.com>
2020-10-25 20:21:16 +0530

    .

--- dry_run.py
+++ dry_run.py
@@ -1,32 +1,38 @@
 import os
 
 
-stack = []
-line_num = [0]
-calc_text = ['']
-print_string = ['']
+class Debugger:
+    def __init__(self):
+        self.stack = []
+        self.line_num = 0
+        self.calc_text = ''
+        self.print_string = ''
+        self.code_lines = []
+
+    def render(self):
+        os.system('cls')
+
+        print('• CODE')
+        for code_line_number, code_line in enumerate(self.code_lines):
+            print('→ ' if code_line_number == self.line_num else '  ',
+                  end='')
+            print(code_line)
+
+        print('\n• CALCULATIONS')
+        print(self.calc_text)
+
+        print('\n• VARIABLES')
+        if self.stack:
+            for key, value in self.stack[-1].items():
+                print(f'{key} = {value}')
+
+        print('\n• PRINT OUTPUT')
+        print(self.print_string)
+
+        print('\n• RECURSION STACK')
+        print(self.stack)
+
+        input()
 
 
-def render():
-    os.system('cls')
-
-    print('• CODE')
-    for code_line_number, code_line in enumerate(code_lines):
-        print('→ ' if code_line_number == line_num[0] else '  ', end='')
-        print(code_line)
-
-    print('\n• CALCULATIONS')
-    print(calc_text[0])
-
-    print('\n• VARIABLES')
-    if stack:
-        for key, value in stack[-1].items():
-            print(f'{key} = {value}')
-
-    print('\n• PRINT OUTPUT')
-    print(print_string[0])
-
-    print('\n• RECURSION STACK')
-    print(stack)
-
-    input()
+dry_run = Debugger()
